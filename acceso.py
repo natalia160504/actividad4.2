7fedc114b070fc6682a45ee884cb2f91fb0a5094
natalia160504 <a395063@uach.mx>
2026-09-21 23:35:29 -0600

    Allow age input from 0 and add identification check

--- acceso.py
+++ acceso.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title ("control de acceso")
 
-edad= st.number_input("Edad:",min_value=18, value=18)
+edad= st.number_input("Edad:",min_value=0, value=18)
+
+tiene_identificacion=st.checkbox("Tiene identificacion")
+
+if edad >=18 and tiene_identificacion:
+  st.write("Puede ingresar.")
+else:
+  st.write("No puede ingrsar")
